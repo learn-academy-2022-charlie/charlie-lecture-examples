a7322b01abc752c3143f4f22b33822dd9bd226b8
WalkerTexas-coder <austinsrwalker@gmail.com>
2022-05-23 11:43:01 -0700

    Create React App Lecture

---
+++ cra-austin-charlean/src/components/Car.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class Car extends Component {
+  render() {
+    return (
+      <>
+      <h2>List of fun cars</h2>
+        <ul>
+          <li>Subaru WRX</li>
+          <li>Subaru Outback</li>
+          <li>Toy Car</li>
+          <li>Barbie Jeep</li>
+        </ul>
+      </>
+    )
+  }
+}
+export default Car
